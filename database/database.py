ec7ab4a5494c62459c3b1fecdb727256ae125d99
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2025-01-06 22:21:26 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,20 +1,21 @@
 import motor.motor_asyncio
+import base64
 from config import DB_URI, DB_NAME
+from datetime import datetime, timedelta
 
 dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
 user_data = database['users']
 channels_collection = database["channels"]
+encoded_links_collection = database["links"]
 
 async def add_user(user_id: int):
-    # Check if the user already exists in the database
     existing_user = await user_data.find_one({'_id': user_id})
     if existing_user:
-        return  # Simply skip adding if user already exists
+        return 
     
     try:
-        # If the user doesn't exist, insert a new record
         await user_data.insert_one({'_id': user_id})
     except Exception as e:
         print(f"Error adding user {user_id}: {e}")
@@ -37,23 +38,52 @@ async def is_admin(user_id: int):
 
 ##-------------------------------------------------------------------
 
-# Save a channel_id to the database
 async def save_channel(channel_id: int):
     """Save a channel_id to the database with invite link expiration."""
     await channels_collection.update_one(
         {"channel_id": channel_id},
-        {"$set": {"channel_id": channel_id, "invite_link_expiry": None}},  # No expiry initially
+        {"$set": {"channel_id": channel_id, "invite_link_expiry": None}}, 
         upsert=True
     )
 
 async def get_channels():
     """Get all channels from the database (No limit)"""
-    channels = await channels_collection.find().to_list(None)  # Fetch all channels
+    channels = await channels_collection.find().to_list(None) 
     return [channel["channel_id"] for channel in channels]
 
-# Delete a channel from the database
+
 async def delete_channel(channel_id: int):
     """Delete a channel from the database"""
     await channels_collection.delete_one({"channel_id": channel_id})
 
 ##-------------------------------------------------------------------
+
+async def save_encoded_link(channel_id: int):
+    encoded_link = base64.urlsafe_b64encode(str(channel_id).encode()).decode()
+    await channels_collection.update_one(
+        {"channel_id": channel_id},
+        {"$set": {"encoded_link": encoded_link, "status": "active"}},
+        upsert=True
+    )
+    return encoded_link
+
+
+async def get_channel_by_encoded_link(encoded_link: str):
+    channel = await channels_collection.find_one({"encoded_link": encoded_link, "status": "active"})
+    return channel["channel_id"] if channel else None
+
+#----------------------------------------------------------------------------------------------------------------
+
+async def save_encoded_link2(channel_id: int, encoded_link: str):
+    await channels_collection.update_one(
+        {"channel_id": channel_id},
+        {"$set": {"req_encoded_link": encoded_link, "status": "active"}},
+        upsert=True
+    )
+    return encoded_link
+
+async def get_channel_by_encoded_link2(encoded_link: str):
+    channel = await channels_collection.find_one({"req_encoded_link": encoded_link, "status": "active"})
+    return channel["channel_id"] if channel else None
+
+
