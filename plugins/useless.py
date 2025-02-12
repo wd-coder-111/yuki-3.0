3bd0f2669095748d83b942b7bb2ee146413a60e8
Nozel Silva <97865856+Sahil0976@users.noreply.github.com>
2025-02-12 16:47:44 +0530

    Create useless.py

---
+++ plugins/useless.py
@@ -0,0 +1,13 @@
+from bot import Bot
+from pyrogram.types import Message
+from pyrogram import filters
+from config import OWNER_ID, BOT_STATS_TEXT, USER_REPLY_TEXT
+from datetime import datetime
+from helper_func import get_readable_time
+
+@Bot.on_message(filters.command('stats') & filters.user(OWNER_ID))
+async def stats(bot: Bot, message: Message):
+    now = datetime.now()
+    delta = now - bot.uptime
+    time = get_readable_time(delta.seconds)
+    await message.reply(BOT_STATS_TEXT.format(uptime=time))
