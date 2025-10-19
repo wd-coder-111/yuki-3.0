c39092cf36d31d6a19a8d13e028d9d7ae8961999
wd-coder-111 <adugaming2829@gmail.com>
2025-10-19 14:30:47 +0530

    Update About Us section in start.py

--- plugins/start.py
+++ plugins/start.py
@@ -156,7 +156,7 @@ async def help_callback(client: Bot, callback_query):
     
     
     await callback_query.answer()
-    await callback_query.message.edit_text("<b><i>About Us..\n\n‣ Made for : @Anime_X_Hunters\n‣ Owned by : @Okabe_xRintarou\n‣ Developer : @The_Seishiro_Nagi\n\n Adios !!</i></b>", reply_markup=inline_buttons)
+    await callback_query.message.edit_text("<b><i>About Us..\n\n‣ Made for : @CrunchyrollJunction\n‣ Owned by : @Demon_Lord_111\n\n Adios !!</i></b>", reply_markup=inline_buttons)
 
 @Bot.on_callback_query(filters.regex("close"))
 async def close_callback(client: Bot, callback_query):
